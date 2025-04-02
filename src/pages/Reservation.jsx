63bc90d4df00969b1e3a0b23b347fc96b811b765
Jinyeong Choi <choiwlsd@gmail.com>
2025-04-03 02:22:37 +0900

    Chore: change styled component loc

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -2,6 +2,71 @@ import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import UserHeader from "../components/Header/UserHeader";
 
+const Reservation = () => {
+  const location = useLocation();
+  const { date, startTime, endTime } = location.state || {};
+  return (
+    <>
+      <UserHeader />
+      <Container>
+        {date ? (
+          <div>
+            <Title>강의실 대여 신청</Title>
+            <InfoContainer>
+              <InfoRow>
+                <Label>신청 강의실</Label>
+                <span>103호</span>
+              </InfoRow>
+              <DoubleInfoRow>
+                <InfoBlock>
+                  <Label>신청 날짜</Label>
+                  <span>{date}</span>
+                </InfoBlock>
+                <InfoBlock>
+                  <Label>신청 시간</Label>
+                  <span>
+                    {Math.floor(startTime)}:
+                    {startTime % 1 === 0.5 ? "30" : "00"}
+                    부터 {Math.floor(endTime)}:
+                    {endTime % 1 === 0.5 ? "30" : "00"}
+                    까지
+                  </span>
+                </InfoBlock>
+              </DoubleInfoRow>
+              <DoubleInfoRow>
+                <InfoBlock>
+                  <Label>성명</Label>
+                  <span>000</span>
+                </InfoBlock>
+                <InfoBlock>
+                  <Label>전화번호</Label>
+                  <span>010-0000-0000</span>
+                </InfoBlock>
+              </DoubleInfoRow>
+
+              <InfoRow>
+                <Label>첨부파일</Label>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <span>강의실대여_신청서.pdf 강의실대여_신청서.hwp</span>
+                  <FileUploadButton>파일 업로드</FileUploadButton>
+                </div>
+              </InfoRow>
+            </InfoContainer>
+            <ButtonContainer>
+              <Button>목록</Button>
+              <Button primary>신청</Button>
+            </ButtonContainer>
+          </div>
+        ) : (
+          <alert>예약 정보가 없습니다.</alert>
+        )}
+      </Container>
+    </>
+  );
+};
+
+export default Reservation;
+
 const Container = styled.div`
   width: 200%;
   margin: 50px auto;
@@ -48,12 +113,6 @@ const Label = styled.span`
   margin-right: 30px;
 `;
 
-const FileSection = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`;
-
 const FileUploadButton = styled.button`
   background: #444;
   color: white;
@@ -79,91 +138,3 @@ const Button = styled.button`
   background: ${(props) => (props.primary ? "#1d2951" : "#555")};
   color: white;
 `;
-
-const Reservation = () => {
-  const location = useLocation();
-  const { date, startTime, endTime } = location.state || {};
-  return (
-    <>
-      <UserHeader />
-      <Container>
-        <Title>강의실 대여 신청</Title>
-        <InfoContainer>
-          <InfoRow>
-            <Label>신청 강의실</Label>
-            <span>103호</span>
-          </InfoRow>
-          <DoubleInfoRow>
-            <InfoBlock>
-              <Label>신청 날짜</Label>
-              <span>{date}</span>
-            </InfoBlock>
-            <InfoBlock>
-              <Label>신청 시간</Label>
-              <span>
-                {Math.floor(startTime)}:{startTime % 1 === 0.5 ? "30" : "00"}
-                부터 {Math.floor(endTime)}:{endTime % 1 === 0.5 ? "30" : "00"}
-                까지
-              </span>
-            </InfoBlock>
-          </DoubleInfoRow>
-          <DoubleInfoRow>
-            <InfoBlock>
-              <Label>성명</Label>
-              <span>000</span>
-            </InfoBlock>
-            <InfoBlock>
-              <Label>전화번호</Label>
-              <span>010-0000-0000</span>
-            </InfoBlock>
-          </DoubleInfoRow>
-
-          <FileSection>
-            <Label>첨부파일</Label>
-            <span>강의실대여_신청서.pdf 강의실대여_신청서.hwp</span>
-            <FileUploadButton>파일 업로드</FileUploadButton>
-          </FileSection>
-        </InfoContainer>
-        <ButtonContainer>
-          <Button>목록</Button>
-          <Button primary>신청</Button>
-        </ButtonContainer>
-      </Container>
-    </>
-  );
-};
-
-export default Reservation;
-
-// const Reservation = () => {
-//   const location = useLocation();
-//   const { date, startTime, endTime } = location.state || {};
-
-//   return (
-//     <div>
-//       <UserHeader />
-//       <h2>강의실 대여 신청</h2>
-//       <hr />
-//       {date ? (
-//         <p>
-//           날짜: {date} <br />
-//           시간: {Math.floor(startTime)}:{startTime % 1 === 0.5 ? "30" : "00"} -{" "}
-//           {Math.floor(endTime)}:{endTime % 1 === 0.5 ? "30" : "00"}
-//         </p>
-//       ) : (
-//         <p>예약 정보가 없습니다.</p>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Reservation;
-
-// const PageWrapper = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   height: 100vh; /* 뷰포트 전체 높이를 차지 */
-//   width: 100vw; /* 뷰포트 전체 너비를 차지 */
-//   margin-top: 20px;
-// `;
