276d0aa3120902a20025b89ef72fb6b71a44ef67
이정빈 <wjd_bin217@naver.com>
2025-04-04 20:22:14 +0900

    문서보관함 밑에 박스

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import CollegeList from '../components/CollegeList';
 import Header from '../components/Header';
-import CollegeList from '../components/CollegeList';
-import UserHeader from '../components/Header/UserHeader';
 
 const Home = () => {
   return (
