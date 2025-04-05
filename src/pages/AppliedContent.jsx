d4154e1e2baa23f27a95daa429e47794f0e211b7
이정빈 <wjd_bin217@naver.com>
2025-04-06 07:49:26 +0900

    강의실대여신청 디자인

--- src/pages/AppliedContent.jsx
+++ src/pages/AppliedContent.jsx
@@ -1,12 +1,12 @@
-import { useNavigate, useLocation } from "react-router-dom";
-import styled from "styled-components";
-import Header from "../components/Header";
-import { useRef, useState, useEffect, useContext } from "react";
-import { UserContext } from "../context/userContext";
-import { VscClose } from "react-icons/vsc";
-import axios from "axios";
-import Modal from "../components/Modal";
-import RejectModal from "../components/RejectModal";
+import { useNavigate, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+import Header from '../components/Header';
+import { useRef, useState, useEffect, useContext } from 'react';
+import { UserContext } from '../context/userContext';
+import { VscClose } from 'react-icons/vsc';
+import axios from 'axios';
+import Modal from '../components/Modal';
+import RejectModal from '../components/RejectModal';
 
 const AppliedContent = () => {
   const location = useLocation();
@@ -14,8 +14,8 @@ const AppliedContent = () => {
 
   const item = location.state?.item;
   const applicationId = item.applicationId;
-  console.log("item", item);
-  console.log("applicationId", applicationId);
+  console.log('item', item);
+  console.log('applicationId', applicationId);
   const [applicationDetail, setApplicationDetail] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -32,41 +32,41 @@ const AppliedContent = () => {
   const fetchApplicationDetail = async () => {
     try {
       const response = await axios.get(
-        "https://itsmeweb.store/api/application/detail",
+        'https://itsmeweb.store/api/application/detail',
         {
           params: { applicationId },
-          headers: { accept: "application/json" },
+          headers: { accept: 'application/json' },
         }
       );
-      console.log("response.data", response.data);
-      if (response.data.result === "SUCCESS") {
+      console.log('response.data', response.data);
+      if (response.data.result === 'SUCCESS') {
         const detail = response.data.data;
-        console.log("detail", detail);
+        console.log('detail', detail);
         setApplicationDetail(detail);
       } else {
-        console.error("에러 발생:", response.data.error);
+        console.error('에러 발생:', response.data.error);
       }
     } catch (error) {
-      console.error("요청 실패:", error);
+      console.error('요청 실패:', error);
     }
   };
 
-  console.log("applicationDetail", applicationDetail);
+  console.log('applicationDetail', applicationDetail);
 
   // 승인 처리 함수
   const handleAdminApproval = async () => {
-    console.log("Approval button clicked");
+    console.log('Approval button clicked');
     try {
       const response = await axios.post(
         `https://itsmeweb.store/api/application/approve?applicationId=${applicationId}`,
         {},
         {
-          headers: { accept: "application/json" },
+          headers: { accept: 'application/json' },
         }
       );
-      console.log("Approval response:", response.data);
+      console.log('Approval response:', response.data);
 
-      if (response.data.result === "SUCCESS") {
+      if (response.data.result === 'SUCCESS') {
         setIsModalOpen(true); // 승인 후 모달 오픈
         fetchApplicationDetail(); // 데이터 갱신
       } else {
@@ -80,100 +80,125 @@ const AppliedContent = () => {
   return (
     <>
       <Header role={userRole} />
-      {isModalOpen && (
-        <Modal
-          title="승인 완료"
-          message="신청이 승인되었습니다."
-          onClose={() => setIsModalOpen(false)}
-        />
-      )}
-      <Container>
-        {/* 이미지 미리보기 */}
-        <PreviewWrapper>
-          {applicationDetail && (
-            <PreviewImage
-              src={applicationDetail.applicationUrl}
-              alt="신청서 이미지"
-            />
-          )}
-        </PreviewWrapper>
-        {/* 신청 상세 정보 */}
-        {applicationDetail && (
-          <>
-            <Title>강의실 대여 신청</Title>
-            <InfoContainer>
-              <InfoRow>
-                <Label>구분</Label> 대여
-              </InfoRow>
-              <InfoRow>
-                <Label>사용 날짜</Label> {applicationDetail.applicationUseDate}
-              </InfoRow>
-              <DoubleInfoRow>
-                <InfoBlock>
-                  <Label>신청 강의실</Label> {applicationDetail.classroom}
-                </InfoBlock>
-                <InfoBlock>
-                  <Label>신청 시간</Label>
-                  {new Date(
-                    `1970-01-01T${applicationDetail.applicationStart}`
-                  ).toLocaleTimeString("ko-KR", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    hour12: true,
-                  })}
-                  부터{" "}
-                  {new Date(
-                    `1970-01-01T${applicationDetail.applicationEnd}`
-                  ).toLocaleTimeString("ko-KR", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    hour12: true,
-                  })}
-                  까지
-                </InfoBlock>
-              </DoubleInfoRow>
-              <DoubleInfoRow>
-                <InfoBlock>
-                  <Label>신청자</Label> {applicationDetail.userName}
-                </InfoBlock>
-                <InfoBlock>
-                  <Label>전화번호</Label> {applicationDetail.userNumber}
-                </InfoBlock>
-              </DoubleInfoRow>
-              <InfoRow>
-                <Label>결과</Label>{" "}
-                {applicationDetail.applicationStatus === "PENDING"
-                  ? "승인 대기"
-                  : applicationDetail.applicationStatus === "APPROVED"
-                  ? "승인"
-                  : applicationDetail.applicationStatus === "REJECTED"
-                  ? "반려"
-                  : ""}
-              </InfoRow>
-              {userRole === "ADMIN" && applicationDetail && (
-                <ButtonContainer>
-                  <Button primary onClick={handleAdminApproval}>
-                    승인
-                  </Button>
-                  <Button>반려</Button>
-                </ButtonContainer>
-              )}
-              {applicationDetail.applicationRejectReason && (
-                <InfoRow>
-                  <Label>반려 사유</Label>{" "}
-                  {applicationDetail.applicationRejectReason}
-                </InfoRow>
-              )}
-            </InfoContainer>
-          </>
+      <EntireWrapper>
+        {isModalOpen && (
+          <Modal
+            title="승인 완료"
+            message="신청이 승인되었습니다."
+            onClose={() => setIsModalOpen(false)}
+          />
         )}
-      </Container>
+        <Leftside>
+          {/* 이미지 미리보기 */}
+          <PreviewWrapper>
+            {applicationDetail && (
+              <PreviewImage
+                src={applicationDetail.applicationUrl}
+                alt="신청서 이미지"
+              />
+            )}
+          </PreviewWrapper>
+        </Leftside>
+        <Rightside>
+          {/* 신청 상세 정보 */}
+          {applicationDetail && (
+            <>
+              <Title>강의실 대여 신청</Title>
+              <InfoContainer>
+                <InfoRow>
+                  <Label>구분</Label>
+                  <span style={{ marginLeft: '50px' }}>대여</span>
+                </InfoRow>
+                <InfoRow>
+                  <Label>사용 날짜</Label>
+                  <span style={{ marginLeft: '19px' }}>
+                    {applicationDetail.applicationUseDate}
+                  </span>
+                </InfoRow>
+                <DoubleInfoRow>
+                  <InfoBlock>
+                    <Label>신청 강의실</Label>
+                    <span style={{ marginLeft: '2px' }}>
+                      {applicationDetail.classroom}
+                    </span>
+                  </InfoBlock>
+                  <InfoBlock>
+                    <Label>신청 시간</Label>
+                    {new Date(
+                      `1970-01-01T${applicationDetail.applicationStart}`
+                    ).toLocaleTimeString('ko-KR', {
+                      hour: '2-digit',
+                      minute: '2-digit',
+                      hour12: true,
+                    })}
+                    부터{' '}
+                    {new Date(
+                      `1970-01-01T${applicationDetail.applicationEnd}`
+                    ).toLocaleTimeString('ko-KR', {
+                      hour: '2-digit',
+                      minute: '2-digit',
+                      hour12: true,
+                    })}
+                    까지
+                  </InfoBlock>
+                </DoubleInfoRow>
+                <DoubleInfoRow>
+                  <InfoBlock>
+                    <Label>신청자</Label>
+                    <span style={{ marginLeft: '33px' }}>
+                      {applicationDetail.userName}
+                    </span>
+                  </InfoBlock>
+                  <InfoBlock>
+                    <Label>전화번호</Label> {applicationDetail.userNumber}
+                  </InfoBlock>
+                </DoubleInfoRow>
+                <InfoRow>
+                  <Label>결과</Label>
+                  <span style={{ marginLeft: '45px' }}>
+                    {applicationDetail.applicationStatus === 'PENDING'
+                      ? '승인 대기'
+                      : applicationDetail.applicationStatus === 'APPROVED'
+                      ? '승인'
+                      : applicationDetail.applicationStatus === 'REJECTED'
+                      ? '반려'
+                      : ''}
+                  </span>
+                </InfoRow>
+                {userRole === 'ADMIN' && applicationDetail && (
+                  <ButtonContainer>
+                    <Button $primary onClick={handleAdminApproval}>
+                      승인
+                    </Button>
+                    <Button>반려</Button>
+                  </ButtonContainer>
+                )}
+                {applicationDetail.applicationRejectReason && (
+                  <InfoRow>
+                    <Label>반려 사유</Label>{' '}
+                    {applicationDetail.applicationRejectReason}
+                  </InfoRow>
+                )}
+              </InfoContainer>
+            </>
+          )}
+        </Rightside>
+      </EntireWrapper>
     </>
   );
 };
 
 export default AppliedContent;
-
+const EntireWrapper = styled.div`
+  // display: flex;
+  // height: 90vh;
+  // justify-content: center;
+  // box-sizing: border-box; /* 패딩을 포함한 전체 너비로 계산됨 */
+  // margin: 0 auto; // 가운데 정렬
+  // padding: 0 10%;
+  display: flex;
+  height: 90vh;
+`;
 const PageWrapper = styled.div`
   display: flex;
   justify-content: center;
@@ -181,7 +206,24 @@ const PageWrapper = styled.div`
   height: 100vh;
   width: 100vw;
 `;
-
+const Leftside = styled.div`
+  width: 70vh;
+  padding: 12px;
+  margin-left: 90px;
+  margin-top: 100px;
+`;
+const Rightside = styled.div`
+  width: 100vh;
+  padding: 44px;
+  margin-left: 50px;
+  margin-right: 20px;
+  margin-top: 80px;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  // width: 100%;
+  // height: 100%;
+`;
 const Container = styled.div`
   width: 800px;
   margin: 50px auto;
@@ -190,7 +232,7 @@ const Container = styled.div`
 `;
 
 const Title = styled.h2`
-  margin-bottom: 20px;
+  margin-bottom: 5px;
   padding-bottom: 10px;
   border-bottom: 1px solid #000000;
 `;
@@ -198,7 +240,7 @@ const Title = styled.h2`
 const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 15px;
+  gap: 2px;
   margin-bottom: 20px;
 `;
 
@@ -259,7 +301,7 @@ const Button = styled.button`
   border: none;
   border-radius: 4px;
   cursor: pointer;
-  background: ${(props) => (props.primary ? "#1d2951" : "#555")};
+  background: ${(props) => (props.primary ? '#1d2951' : '#555')};
   color: white;
 `;
 
@@ -292,7 +334,7 @@ const PreviewWrapper = styled.div`
 `;
 
 const PreviewImage = styled.img`
-  max-width: 100%;
+  width: 450px;
   border: 1px solid #ccc;
   border-radius: 4px;
 `;
